86b33ff5b19d48ada3ea17440deeee9de948f6d9
s2003zy@qq.com <s2003zy@qq.com>
2014-06-19 15:56:13 +0800

    ignoreFile

---
+++ DontTouchWhiteBlock/Classes/Block.cpp
@@ -0,0 +1,9 @@
+//
+//  Block.cpp
+//  DontTouchWhiteBlock
+//
+//  Created by ZhiyangSong on 6/18/14.
+//
+//
+
+#include "Block.h"
---
+++ DontTouchWhiteBlock/Classes/Block.h
@@ -0,0 +1,14 @@
+//
+//  Block.h
+//  DontTouchWhiteBlock
+//
+//  Created by ZhiyangSong on 6/18/14.
+//
+//
+
+#ifndef __DontTouchWhiteBlock__Block__
+#define __DontTouchWhiteBlock__Block__
+
+#include <iostream>
+
+#endif /* defined(__DontTouchWhiteBlock__Block__) */
